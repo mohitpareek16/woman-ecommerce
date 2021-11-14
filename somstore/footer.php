918217da4078f7c27f75a7d757b2279c16f20e40
Mohit <mohitpareekyo@gmail.com>
2021-11-14 13:25:59 +0530

    submit

---
+++ somstore/footer.php
@@ -0,0 +1,63 @@
+		<div id="footer">
+			<div class="boxes">
+				<!-- Begin Shell -->
+				<div class="shell">
+					<div class="box post-box">
+						<h2>About SomStore</h2>
+						<div class="box-entry">
+							<img src="images/logo.png" alt="SomStore" width="160" height="80"/>
+							<p>You can be confident when you're shopping online with SomStore. Our Secure online shopping website encrypts your personal and financial information to ensure your order information is protected.We use industry standard 128-bit encryption. Our Secure online shopping website locks all critical information passed from you to us,
+                             such as personal information, in an encrypted envelope, making it extremely difficult for this information to be intercepted. </p>
+							<div class="cl">&nbsp;</div>
+						</div>
+					</div>
+					<div class="box social-box">
+						<h2>We are Social</h2>
+						<ul>
+							<li><a href="#" title="Facebook"><img src="images/social-icon1.png" alt="Facebook" /><span>Facebook</span><span class="cl">&nbsp;</span></a></li>
+							<li><a href="#" title="Twitter"><img src="images/social-icon2.png" alt="Twitter" /><span>Twitter</span><span class="cl">&nbsp;</span></a></li>							
+							<li><a href="#" title="RSS"><img src="images/social-icon4.png" alt="RSS" /><span>RSS</span><span class="cl">&nbsp;</span></a></li>
+							<li><a href="#" title="Blogger"><img src="images/social-icon7.png" alt="Blogger" /><span>Blogger</span><span class="cl">&nbsp;</span></a></li>
+						</ul>
+						<div class="cl">&nbsp;</div>
+					</div>
+					<div class="box">
+						<h2>Information</h2>
+						<ul>
+							<li><a href="#" title="Privacy Policy">Privacy Policy</a></li>
+							<li><a href="#" title="Contact Us">Contact Us</a></li>
+							<li><a href="#" title="Log In">Log In</a></li>
+							<li><a href="#" title="Account">Account</a></li>
+
+						</ul>
+					</div>
+					<div class="box last-box">
+						<h2>Categories</h2>
+						<ul>
+							<li><a href="#" title="Clothes">Clothes</a></li>
+							<li><a href="#" title="Cleaning Material">Cleaning Material</a></li>
+							<li><a href="#" title="Fizzi Drinks">Fizzy Drinks</a></li>
+							<li><a href="#" title="Food Stuff">Food Stuff</a></li>
+						</ul>
+					</div>
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- End Shell -->
+			</div>
+			<div class="copy">
+				<!-- Begin Shell -->
+				<div class="shell">
+					<div class="carts">
+						<ul>
+							<li><span>We accept</span></li>
+							<li><a href="#" title="Zaad service"><img src="images/zaad.png" alt="Zaad Service" /></a></li>
+							<li><a href="#" title="Somstore"><img src="images/suncart.png" alt="Somstore" /></a></li>
+				
+						</ul>
+					</div>	<p>&copy; Somstore.com. Groups <a href="index.php"><i><font color="fefefe"> Welcome To Somstore Online Shopping Site </font></i></a></p>
+					<div class="cl">&nbsp;</div>
+					Copyright © 2014 SomStore.com All rights reserved. The information contained in SomStore.com may not be published, broadcast, rewritten, or redistributed without the prior written authority of SomStore.com
+				</div>
+				<!-- End Shell -->
+			</div>
+		</div>
